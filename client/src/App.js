5963f884c8e58915a5ef52c565eed852168c36f9
joonho_Lee <goodfluence@naver.com>
2021-01-07 08:31:49 +0900

    front & backend basic setup

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import { Component } from "react";
+
+class App extends Component {
+  render(){
+    return (
+      <div className="App">
+        <h1>Home!</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ server.js
@@ -0,0 +1,18 @@
+import bodyParser, { urlencoded } from "body-parser";
+import express from "express";
+import morgan from "morgan";
+
+const app = express();
+app.use(morgan("dev"));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => res.send("consumption!"));
+app.get("/user", (req, res) => res.send("user!"));
+app.get("/login", (req, res) => res.send("login!"));
+app.get("/saving", (req, res) => res.send("saving!"));
+
+
+app.listen(5000, () => console.log("✅Listening on : http://localhost:5000"));
+
+export default app;
