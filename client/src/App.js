6565a823debf60cbc7f5c56698906ee8784d3b8d
joonho_Lee <goodfluence@naver.com>
2021-01-07 10:36:31 +0900

    proxy & concurrently setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,23 @@
 import { Component } from "react";
+import axios from "axios";
 
 class App extends Component {
+  state = {
+      message : ""
+  }
+  
+  async componentDidMount(){
+    const user = await axios.get("/api/hello");
+    const { data : { message} } = user;
+    console.log(message);
+    this.setState({ message });
+  }
+  
   render(){
+    const { message } = this.state;
     return (
       <div className="App">
-        <h1>Home!</h1>
+        <h1>{message}</h1>
       </div>
     );
   }
--- server.js
+++ server.js
@@ -11,7 +11,9 @@ app.get("/", (req, res) => res.send("consumption!"));
 app.get("/user", (req, res) => res.send("user!"));
 app.get("/login", (req, res) => res.send("login!"));
 app.get("/saving", (req, res) => res.send("saving!"));
-
+app.get("/api/hello", (req, res) => res.send({
+    message: "Hello, this is Chris"
+}));
 
 app.listen(5000, () => console.log("✅Listening on : http://localhost:5000"));
 
