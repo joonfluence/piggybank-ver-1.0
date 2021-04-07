68015a49fabfdd51b4e9f1221b8a68aec0e5a3c2
joonho_Lee <goodfluence@naver.com>
2021-04-08 01:28:33 +0900

    BE - 오류 수정

--- routers/apiRouter.js
+++ routers/apiRouter.js
@@ -44,50 +44,51 @@ import {
   deleteGoalInfo,
 } from "../controllers/SavingGoalController.js";
 import routes from "../routes.js";
+import Auth from "../client/src/hoc/auth.js";
 
 const apiRouter = express.Router();
 
 // 1) 회원가입 및 로그인, 로그아웃
 apiRouter.post(routes.join, postJoin);
 apiRouter.post(routes.login, postLogin);
-apiRouter.get(routes.logout, getLogOut);
+apiRouter.get(routes.logout, Auth, getLogOut);
 
 // 2) 인증체크
 apiRouter.get(routes.auth, getAuth);
 
 // 3) 사용자 정보
-apiRouter.get(routes.mypage, getUserInfo);
-apiRouter.put(routes.mypage, editUserInfo);
+apiRouter.get(routes.mypage, Auth, getUserInfo);
+apiRouter.put(routes.mypage, Auth, editUserInfo);
 
 // 4) 소비정보 CRUD
-apiRouter.get(routes.paying, getPayingInfo);
-apiRouter.post(routes.paying, postPayingInfo);
-apiRouter.put(routes.payings(), editPayingInfo);
-apiRouter.delete(routes.payings(), deletePayingInfo);
+apiRouter.get(routes.paying, Auth, getPayingInfo);
+apiRouter.post(routes.paying, Auth, postPayingInfo);
+apiRouter.put(routes.payings(), Auth, editPayingInfo);
+apiRouter.delete(routes.payings(), Auth, deletePayingInfo);
 
 // 5) 저축정보 CRUD
-apiRouter.get(routes.saving, getSavingInfo);
-apiRouter.post(routes.saving, postSavingInfo);
-apiRouter.put(routes.savings(), editSavingInfo);
-apiRouter.delete(routes.savings(), deleteSavingInfo);
+apiRouter.get(routes.saving, Auth, getSavingInfo);
+apiRouter.post(routes.saving, Auth, postSavingInfo);
+apiRouter.put(routes.savings(), Auth, editSavingInfo);
+apiRouter.delete(routes.savings(), Auth, deleteSavingInfo);
 
 // 6) 디테일 정보
-apiRouter.post(routes.payingMonth, getPayingMonth);
-apiRouter.post(routes.savingMonth, getSavingMonth);
-apiRouter.get(routes.payings(), getPayingDetail);
-apiRouter.get(routes.savings(), getSavingDetail);
+apiRouter.post(routes.payingMonth, Auth, getPayingMonth);
+apiRouter.post(routes.savingMonth, Auth, getSavingMonth);
+apiRouter.get(routes.payings(), Auth, getPayingDetail);
+apiRouter.get(routes.savings(), Auth, getSavingDetail);
 
 // 7) 소비 & 저축 카테고리별 정보
-apiRouter.post(routes.categoryPaying, getPayingCategory);
-apiRouter.post(routes.categorySaving, getSavingCategory);
+apiRouter.post(routes.categoryPaying, Auth, getPayingCategory);
+apiRouter.post(routes.categorySaving, Auth, getSavingCategory);
 
 // 8) 예산 정보
-apiRouter.get(routes.budget, getBudgetInfo);
-apiRouter.post(routes.budget, postBudgetInfo);
-apiRouter.put(routes.budgets(), editBudgetInfo);
-apiRouter.delete(routes.budgets(), deleteBudgetInfo);
-apiRouter.get(routes.budgets(), getBudgetDetail);
-apiRouter.post(routes.budgetMonth, getBudgetMonth);
+apiRouter.get(routes.budget, Auth, getBudgetInfo);
+apiRouter.post(routes.budget, Auth, postBudgetInfo);
+apiRouter.put(routes.budgets(), Auth, editBudgetInfo);
+apiRouter.delete(routes.budgets(), Auth, deleteBudgetInfo);
+apiRouter.get(routes.budgets(), Auth, getBudgetDetail);
+apiRouter.post(routes.budgetMonth, Auth, getBudgetMonth);
 
 // 9) 저축 목표 정보
 apiRouter.get(routes.savingGoal, getGoalInfo);
